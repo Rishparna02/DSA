e8a8775ef32891e223773df8008ce6846ed1c0e6
Rishparna02 <109991962+Rishparna02@users.noreply.github.com>
2024-05-08 23:30:59 +0530

    Updated Solution - GfG to GitHub

--- Basic/Peak element/peak-element.java
+++ Basic/Peak element/peak-element.java
@@ -42,21 +42,24 @@ class PeakElement{
 // } Driver Code Ends
 
 
-class Solution {
-    public int peakElement(int[] arr, int n) {
-        int left = 0;
-        int right = n - 1;
+/*Complete the function below*/
 
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] > arr[mid + 1]) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return left;
+class Solution
+{
+	// Function to find the peak element
+	// arr[]: input array
+	// n: size of array a[]
+	public int peakElement(int[] arr,int n)
+    {
+       int start=0, end=n-1, mid;
+       while(start<=end){
+           mid=(start+end)/2;
+           int prev=mid-1>=0?mid-1:0;
+           int next=mid+1<n?mid+1:n-1;
+           if (arr[mid]>=arr[prev] && arr[mid]>=arr[next]) return mid;
+           else if(arr[mid]<arr[next]) start=mid+1;
+           else end=mid-1;
+       }
+       return 0;
     }
 }
