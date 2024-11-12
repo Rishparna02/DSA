519c82be85c8dcd8d2b751a3e406e30eb1117112
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2024-11-12 13:36:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
@@ -19,6 +19,7 @@ class GFG {
             Solution obj = new Solution();
             int ans = obj.numIslands(grid);
             System.out.println(ans);
+            System.out.println("~");
         }
         scanner.close();
     }
@@ -26,37 +27,38 @@ class GFG {
 
 // } Driver Code Ends
 
+
 class Solution {
+     public void traverse(char[][] grid, int x, int y)
+    {
+        if(x>=grid.length || y>=grid[0].length || x<0 || y<0)return;
+        if(grid[x][y]=='1')
+        {
+            grid[x][y]='0';
+            traverse(grid, x+1, y);
+            traverse(grid, x-1, y);
+            traverse(grid, x, y+1);
+            traverse(grid, x, y-1);
+            traverse(grid, x-1, y-1);
+            traverse(grid, x+1, y-1);
+            traverse(grid, x-1, y+1);
+            traverse(grid, x+1, y+1);
+        }
+    }
     public int numIslands(char[][] grid) {
         // Code here
-        int n=grid.length,m=grid[0].length;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
-                    dfs(i,j,grid,n,m);
-                    ans++;
+        int result=0;
+        for(int i=0; i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    result+=1;
+                    traverse(grid, i, j);
                 }
             }
         }
-        
-        return ans;
-    }
-    private void dfs(int i,int j,char[][] grid,int n,int m){
-        if(i>=n || i<0 || j>=m || j<0 || grid[i][j]=='0' || grid[i][j]=='2'){
-            return;
-        }
-        
-        grid[i][j]='2';
-        
-        dfs(i+1,j,grid,n,m); // down
-        dfs(i-1,j,grid,n,m); // up
-        dfs(i,j+1,grid,n,m); // right
-        dfs(i,j-1,grid,n,m); // left
-        //diagonals
-        dfs(i-1,j-1,grid,n,m); // up->left
-        dfs(i-1,j+1,grid,n,m); // up->right
-        dfs(i+1,j-1,grid,n,m); // down->left
-        dfs(i+1,j+1,grid,n,m); // down->right
+        return result;
     }
 }
