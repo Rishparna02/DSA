22988bc83d6981bd550168a88ba6d62c035cb6a9
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2024-11-30 22:58:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -11,48 +11,43 @@ class GFG {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
-            String str = br.readLine();
-            String s1 = str.split(" ")[0];
-            String s2 = str.split(" ")[1];
+            String s1 = br.readLine(); // first string
+            String s2 = br.readLine(); // second string
 
             Solution obj = new Solution();
 
-            if (obj.isAnagram(s1, s2)) {
-                System.out.println("YES");
+            if (obj.areAnagrams(s1, s2)) {
+                System.out.println("true");
             } else {
-                System.out.println("NO");
+                System.out.println("false");
             }
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
+
 class Solution {
     // Function is to check whether two strings are anagram of each other or not.
-    public static boolean isAnagram(String s1, String s2) {
-        s1 = s1.toLowerCase();
-        s2 = s2.toLowerCase();
-        if (s1.length() != s2.length()) {
-            return false;
-        }
-        HashMap<Character, Integer> freq1 = new HashMap<>();
-        HashMap<Character, Integer> freq2 = new HashMap<>();
-        for (char c : s1.toCharArray()) {
-            if (Character.isLetter(c)) {
-                freq1.put(c, freq1.getOrDefault(c, 0) + 1);
-            }
-        }
-        for (char c : s2.toCharArray()) {
-            if (Character.isLetter(c)) {
-                freq2.put(c, freq2.getOrDefault(c, 0) + 1);
-            }
-        }
+    public static boolean areAnagrams(String s1, String s2) {
 
-        if (freq1.equals(freq2)) {
-            return true;
-        } else {
-            return false;
+        // Your code here
+        int n = s1.length(), m = s2.length();
+        if(n != m) return false;
+        int[] hash = new int[26];
+        
+        for(int i = 0; i < n; i++){
+            char ch1 = s1.charAt(i);
+            char ch2 = s2.charAt(i);
+            hash[ch1-'a']++;
+            hash[ch2-'a']--;
         }
+        
+        for(int i = 0; i < 26; i++){
+            if(hash[i] != 0) return false;
+        }
+        
+        return true;
     }
 }
-
