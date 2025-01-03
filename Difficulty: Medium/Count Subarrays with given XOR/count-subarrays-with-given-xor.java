7f5fcba9a802bcb5d2ea2d537b8a85787115a45d
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2025-01-03 23:30:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        while (tc-- > 0) {
+            String s[] = br.readLine().split(" ");
+            int arr[] = new int[s.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = Integer.parseInt(s[i]);
+            }
+            int k = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            System.out.println(obj.subarrayXor(arr, k));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public long subarrayXor(int arr[], int k) {
+        // code here
+        long ans=0;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        map.put(0,1);
+        int currentXor=0;
+
+        for(int num:arr){
+            currentXor^=num;
+            int target=currentXor^k;
+            if(map.containsKey(target)){
+                ans+=map.get(target);
+            }
+            map.put(currentXor,map.getOrDefault(currentXor,0)+1);
+        }
+        return ans;
+    }
+}
