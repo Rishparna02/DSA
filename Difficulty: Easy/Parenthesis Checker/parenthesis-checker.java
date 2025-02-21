85c637590e20b45ec620b03407e51798ea1c47d7
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2025-02-22 00:00:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -1,61 +1,48 @@
 //{ Driver Code Starts
-import java.util.*;
 import java.io.*;
 import java.lang.*;
+import java.util.*;
 
-class Driverclass
-{
-    public static void main(String args[])
-    {
+class Driverclass {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        
-        //Reading total number of testcases
-        int t= sc.nextInt();
-        
-        while(t-- >0)
-        {
-            //reading the string
+
+        // Reading total number of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // reading the string
             String st = sc.next();
-            
-            //calling ispar method of Paranthesis class 
-            //and printing "balanced" if it returns true
-            //else printing "not balanced"
-            if(new Solution().ispar(st) == true)
-                System.out.println("balanced");
+
+            // calling ispar method of Paranthesis class
+            // and printing "balanced" if it returns true
+            // else printing "not balanced"
+            if (new Solution().isBalanced(st) == true)
+                System.out.println("true");
             else
-                System.out.println("not balanced");
-        
+                System.out.println("false");
+
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
-class Solution
-{
-    //Function to check if brackets are balanced or not.
-    static boolean ispar(String x)
-    {
-        // add your code here
-        Stack<Character> st = new Stack<>();
-        for(char ch : x.toCharArray()){
-            if(ch == '{' || ch == '(' || ch == '['){
-                st.push(ch);
-            }
-            else{
-                if(ch == '}') ch = '{';
-                if(ch == ')') ch = '(';
-                if(ch == ']') ch = '[';
-                if(!st.isEmpty() && st.peek() != ch){
-                    return false;
-                }
-                else if(st.isEmpty()){
-                    return false;
-                }
-                else{
-                    st.pop();
-                }
-            }
+
+
+class Solution {
+    static boolean isBalanced(String s) {
+        // code here
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(stack.empty())
+                stack.push(ch);
+            else if(stack.peek()+1==ch||stack.peek()+2==ch)
+                stack.pop();
+            else
+                stack.push(ch);
         }
-        return st.isEmpty();
+        return stack.empty();
     }
 }
