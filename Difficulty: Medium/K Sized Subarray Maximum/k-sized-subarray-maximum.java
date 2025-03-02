3087c4c0dd3fe91cfbc6097c413aa12d2787d33f
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2025-03-02 10:55:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -13,7 +13,6 @@ public class Main {
 
         while (t-- > 0) {
             // taking total number of elements
-            int k = Integer.parseInt(br.readLine());
             String line = br.readLine();
             String[] tokens = line.split(" ");
 
@@ -28,48 +27,47 @@ public class Main {
             int[] arr = new int[array.size()];
             int idx = 0;
             for (int i : array) arr[idx++] = i;
-            ArrayList<Integer> res = new Solution().max_of_subarrays(k, arr);
+            int k = Integer.parseInt(br.readLine());
+            ArrayList<Integer> res = new Solution().maxOfSubarrays(arr, k);
 
             // printing the elements of the ArrayList
             for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
             System.out.println();
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
 
-// User function template for JAVA
+
 
 class Solution {
-    // Function to find maximum of each subarray of size k.
-    public ArrayList<Integer> max_of_subarrays(int k, int arr[]) {
-        // Your code here
-         ArrayList<Integer> list = new ArrayList<Integer>();
-        
-        int tempMax= findMax(0,k-1,arr);
-        list.add(tempMax);
-        
-        for(int i=1; i<=arr.length-k; i++){
-            if(tempMax == arr[i-1] ){
-                tempMax = findMax(i, i+k-1,arr);
-                list.add(tempMax);
+    public ArrayList<Integer> maxOfSubarrays(int arr[], int k) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Deque<Integer> dq = new LinkedList<>();
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            // Remove out-of-window elements
+            if (!dq.isEmpty() && dq.peek() == i - k) {
+                dq.poll();
             }
-            else{
-                    tempMax = Math.max(tempMax, arr[i+k-1]);
-                    list.add(tempMax);
+
+            // Remove smaller elements in window
+            while (!dq.isEmpty() && arr[dq.peekLast()] <= arr[i]) {
+                dq.pollLast();
+            }
+
+            // Add current index
+            dq.offer(i);
+
+            // Store max for current window
+            if (i >= k - 1) {
+                res.add(arr[dq.peek()]);
             }
         }
-        return list;
-        
-    }
-    
-    public int findMax(int start, int end, int arr[]){
-        int max=0;
-        while(start <= end){
-            max = Math.max(max, arr[start]);
-            start++;
-        }
-        return max;
+
+        return res;
     }
 }
