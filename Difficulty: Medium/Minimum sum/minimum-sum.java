93a67a4dc6de67340a4d8b52bb8b14325220141a
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2025-06-23 22:58:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum sum/minimum-sum.java
+++ Difficulty: Medium/Minimum sum/minimum-sum.java
@@ -1,75 +1,32 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String arr[] = br.readLine().split(" ");
-            int a[] = new int[arr.length];
-
-            for (int i = 0; i < arr.length; i++) {
-                a[i] = Integer.parseInt(arr[i]);
-            }
-            Solution obj = new Solution();
-            int f = 0;
-            String A = obj.minSum(a);
-            System.out.println(A);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     String minSum(int[] arr) {
         // code here
+        int n = arr.length;
         Arrays.sort(arr);
-        StringBuilder num1 = new StringBuilder();
-        StringBuilder num2 = new StringBuilder();
-        boolean first = true;
-        for(int i:arr){
-            if(i==0) continue;
-            if(first){
-                num1.append(i+"");
-            }
-            else num2.append(i+"");
-            first = !first;
-        }
-        return solve(num1,num2);
-    }
-    static String solve(StringBuilder num1,StringBuilder num2){
         StringBuilder ans = new StringBuilder();
-        int carry = 0,n1 = num1.length()-1,n2 = num2.length()-1;
-        while(n1>=0 && n2>=0){
-            int e = (num1.charAt(n1)-'0')+(num2.charAt(n2)-'0')+carry;
-            ans.append((e%10)+"");
-            carry = e/10;
-            n1--;
-            n2--;
+        int [] first = new int[n %2 == 0 ? n/2 : n/2+ 1];
+        
+        int i, j = first.length-1, carry = 0;
+        for (i = n-1; i > 0; i-=2){
+            int a = arr[i] + arr[i-1] + carry;
+            first[j] = a % 10;
+            carry = a / 10;
+            j--;
         }
-        while(n1>=0){
-            int e = (num1.charAt(n1)-'0')+carry;
-            ans.append((e%10)+"");
-            carry = e/10;
-            n1--;
+        
+        if (n % 2 != 0){
+            first[0] = (arr[0] + carry) % 10;
+            carry = (arr[0] + carry) / 10;
         }
-        while(n2>=0){
-            int e = (num2.charAt(n2)-'0')+carry;
-            ans.append((e%10)+"");
-            carry = e/10;
-            n2--;
+        if (carry != 0)
+            ans.append(Integer.valueOf(carry));
+        for (i = 0; i < first.length; i++){
+            if (first[i] != 0)
+                break;
         }
-        if(carry>=1) ans.append(1+"");
-        return ans.reverse().toString();
+        for (i = i; i < first.length; i++){
+            ans.append(Integer.valueOf(first[i]));
+        }
+        return ans.toString();
     }
 }
