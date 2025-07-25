45ba59ded144b2fc5321b0f5b3e059f1d4acd1ee
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2025-07-25 23:14:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.java
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.java
@@ -1,55 +1,26 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Sorting {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().circularSubarraySum(arr));
-            // System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
+    public int maxCircularSum(int arr[]) {
+        // code here
+        int total = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int curMax = 0;
+        int minSum = Integer.MAX_VALUE;
+        int curMin = 0;
 
-    // a: input array
-    // n: size of array
-    // Function to find maximum circular subarray sum.
-    public int circularSubarraySum(int arr[]) {
+        for (int num : arr) {
+            curMax = Math.max(num, curMax + num);
+            maxSum = Math.max(maxSum, curMax);
 
-        // Your code here
-        int maxSum=Integer.MIN_VALUE;
-        int minSum=Integer.MAX_VALUE;
-        int curr=0,currneg=0;
-        int s=0;
-        for(int i:arr)
-        {
-            s+=i;
-            curr+=i;
-            maxSum= Math.max(curr,maxSum);
-            if(curr<0)
-            {
-                curr=0;
-            }
-            currneg+=i;
-            minSum=Math.min(currneg,minSum);
-            if(currneg>0)
-            {
-                currneg=0;
-            }
+            curMin = Math.min(num, curMin + num);
+            minSum = Math.min(minSum, curMin);
+
+            total += num;
         }
-        if(s==minSum)
-        {
+
+        if (maxSum < 0) {
             return maxSum;
         }
-        return Math.max(maxSum,s-minSum);
+
+        return Math.max(maxSum, total - minSum);
     }
 }
