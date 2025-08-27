6a7f971241de5bdd3fe12f0c43f05e058134192f
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2025-08-27 23:37:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
@@ -1,59 +1,26 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-            System.out.println(new Solution().countTriangles(arr));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    // Function to count the number of possible triangles.
-    static int countTriangles(int arr[]) {
+    public int countTriangles(int arr[]) {
         // code here
-        int n = arr.length;
-
-        // If there are fewer than 3 sides, no triangle can be formed.
-        if (n < 3) return 0;
-
-        // Sort the array in ascending order
+          
+        int count=0;
+        if(arr.length<3) return 0;
         Arrays.sort(arr);
-
-        int count = 0;
-
-        // Fix the third side (largest side of the triangle)
-        for (int k = n - 1; k >= 2; k--) {
-            int i = 0; // Start pointer
-            int j = k - 1; // End pointer before the largest side
-
-            // Check pairs (i, j) such that arr[i] + arr[j] > arr[k]
-            while (i < j) {
-                if (arr[i] + arr[j] > arr[k]) {
-                    // All pairs between i and j can form a triangle with arr[k]
-                    count += (j - i);
-                    j--; // Decrease j to check smaller pairs
-                } else {
-                    i++; // Increase i to increase the sum
+        
+        for(int i=arr.length-1;i>=2;i--){
+            
+            int left=0;
+            int right=i-1;
+            while(left<right){
+                if(arr[left]+arr[right]>arr[i]){
+                    count+=right-left;
+                    right--;
+                }
+                else{
+                    left++;
                 }
             }
         }
-
+        
         return count;
     }
 }
